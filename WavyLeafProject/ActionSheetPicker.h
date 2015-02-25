2bb091f8103cd51b70f5f8395d57d50f33c0c9a9
Jal Irani <jayj8892@Jals-MacBook-Air.local>
2015-02-24 19:33:16 -0500

    Fixed ActionSheet

--- WavyLeafProject/ActionSheetPicker.h
+++ WavyLeafProject/ActionSheetPicker.h
@@ -25,36 +25,10 @@
 //SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-#import <Foundation/Foundation.h>
-
-
-@interface AbstractActionSheetPicker : NSObject
-@property (nonatomic, copy) NSString *title;
-@property (nonatomic, strong) UIView *pickerView;
-@property (nonatomic, readonly) CGSize viewSize;
-@property (nonatomic, strong) NSMutableArray *customButtons;
-@property (nonatomic, assign) BOOL hideCancel;
-@property (nonatomic, assign) CGRect presentFromRect;
-
-// For subclasses.
-- (id)initWithTarget:(id)target successAction:(SEL)successAction cancelAction:(SEL)cancelActionOrNil origin:(id)origin;
-
-// Present the ActionSheetPicker
-- (void)showActionSheetPicker;
-
-// For subclasses.  This is used to send a message to the target upon a successful selection and dismissal of the picker (i.e. not canceled).
-- (void)notifyTarget:(id)target didSucceedWithAction:(SEL)successAction origin:(id)origin;
-
-// For subclasses.  This is an optional message upon cancelation of the picker.
-- (void)notifyTarget:(id)target didCancelWithAction:(SEL)cancelAction origin:(id)origin;
-
-// For subclasses.  This returns a configured picker view.  Subclasses should autorelease.
-- (UIPickerView *)configuredPickerView;
-
-// Adds custom buttons to the left of the UIToolbar that select specified values
-- (void)addCustomButtonWithTitle:(NSString *)title value:(id)value;
-
-//For subclasses. This responds to a custom button being pressed.
-- (IBAction)customButtonPressed:(id)sender;
-
-@end
+#import "ActionSheetCustomPickerDelegate.h"
+#import "AbstractActionSheetPicker.h"
+#import "ActionSheetCustomPicker.h"
+#import "ActionSheetDatePicker.h"
+#import "ActionSheetDistancePicker.h"
+#import "ActionSheetLocalePicker.h"
+#import "ActionSheetStringPicker.h"
